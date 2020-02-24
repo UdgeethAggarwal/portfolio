24bf12748f9be6a529fee05f9f741ceb78cae6f7
Udgeeth27 <udgeeth.aggarwal@gmail.com>
2020-02-24 19:05:11 +0530

    Adding new build

---
+++ precache-manifest.fd9d64aaf930b910d1a7213b70659aeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "69c8bfe96be2a2b3bbf4",
+    "url": "/portfolio/static/js/main.69c8bfe9.chunk.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/portfolio/static/js/2.477d3cfd.chunk.js"
+  },
+  {
+    "revision": "69c8bfe96be2a2b3bbf4",
+    "url": "/portfolio/static/css/main.fd5f9a01.chunk.css"
+  },
+  {
+    "revision": "f759f54969914a525036c62fa3fb9b3d",
+    "url": "/portfolio/index.html"
+  }
+];
