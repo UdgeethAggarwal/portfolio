020c0f0a3e27c416cd705e850ea607400029d305
Udgeeth27 <udgeeth.aggarwal@gmail.com>
2020-02-25 12:12:45 +0530

    Adding files for build

---
+++ precache-manifest.5881adb1fd8ab75ddaf8b12932521489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "1093e91ab31e1a93cb98",
+    "url": "/portfolio/static/js/main.1093e91a.chunk.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/portfolio/static/js/2.477d3cfd.chunk.js"
+  },
+  {
+    "revision": "1093e91ab31e1a93cb98",
+    "url": "/portfolio/static/css/main.fd5f9a01.chunk.css"
+  },
+  {
+    "revision": "0d3f3c3a61ce82477933c43adaeb5533",
+    "url": "/portfolio/index.html"
+  }
+];
